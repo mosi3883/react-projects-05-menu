48c42bbe2925e98242076f6a388129336c50f806
Mostafa <mosi3883@gmail.com>
2022-05-02 20:05:14 +0430

    update to react@18.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
-const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+const categories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
 
   const filterItems = (category) => {
     if (category === 'all') {
